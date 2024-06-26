4253648e6171a60435648287f121fc8cfb3b4a67
azizedogan <azizedgn.a@outlook.com>
2024-06-26 21:38:42 +0300

    app.js düzenlendi

--- app.js
+++ app.js
@@ -62,32 +62,3 @@ form.addEventListener('submit',function(e){  // form elemanlarının durumunu ko
   checkPhone(phone);
 });
 
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: "auto",
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
